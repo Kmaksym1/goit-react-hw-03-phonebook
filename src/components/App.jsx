0752f8be8e34e221da40a987e6daadba996a63a1
Maksym Kravchenko <kmaksym@gmail.com>
2023-06-21 18:57:46 -0700

    21/06 FIRST ATTTEMP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ class App extends Component {
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      
     ],
     filter: "",
   };
